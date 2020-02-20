5a418903ec00d5469d0b34b60de9d06a463e73df
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-02-20 20:21:01 +0500

    Учебный проект: pimp my mage

---
+++ js/backend.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function () {
+  window.save = function (data, onSuccess) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      onSuccess(xhr.response);
+    });
+
+    xhr.open('POST', window.util.URL_S);
+    xhr.send(data);
+  };
+
+  window.load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === window.util.statusCode) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = window.util.timeOut;
+    xhr.open('GET', window.util.URL_L);
+    xhr.send();
+  };
+})();
--- js/util.js
+++ js/util.js
@@ -3,6 +3,10 @@
 (function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
+  var URL_S = 'https://js.dump.academy/code-and-magick';
+  var URL_L = 'https://js.dump.academy/code-and-magick/data';
+  var TIMEOUT = 10000;
+  var statusCode = 200;
 
   window.util = {
     isEscEvent: function (evt, action) {
@@ -14,6 +18,10 @@
       if (evt.keyCode === ENTER_KEYCODE) {
         action();
       }
-    }
+    },
+    URL_S: URL_S,
+    URL_L: URL_L,
+    timeOut: TIMEOUT,
+    statusCode: statusCode
   };
 })();
